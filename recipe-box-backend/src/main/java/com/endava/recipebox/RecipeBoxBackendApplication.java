2cbfee16502a73c557eceae22f62a5dc25d50d1d
Dragos Dan Dordea <Dragos.Dordea@endava.com>
2024-08-01 18:34:09 +0300

    Initial commit

---
+++ recipe-box-backend/src/main/java/com/endava/recipebox/RecipeBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.endava.recipebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBoxBackendApplication.class, args);
+	}
+
+}
---
+++ recipe-box-backend/src/test/java/com/endava/recipebox/RecipeBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.recipebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBoxBackendApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
