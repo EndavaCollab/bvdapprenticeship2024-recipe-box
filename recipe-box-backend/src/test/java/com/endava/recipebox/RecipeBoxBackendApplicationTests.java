b9ea6042eaaea77e5c0da0c6f17f7a8a8dd85ec1
Ionel14 <53469111+Ionel14@users.noreply.github.com>
2024-08-12 00:03:15 +0300

    tested my login endpoint

--- recipe-box-backend/src/test/java/com/endava/recipebox/RecipeBoxBackendApplicationTests.java
+++ recipe-box-backend/src/test/java/com/endava/recipebox/RecipeBoxBackendApplicationTests.java
@@ -1,13 +1,41 @@
 package com.endava.recipebox;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RecipeBoxBackendApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    // CREATE User
+    @Test
+    public void shouldSaveAndReturnUserIfSuccessful() throws Exception {
+        String username = "wew12";
+        this.mockMvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON)
+                        .content(username))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value(username));
+
+        this.mockMvc.perform(get("/login"))
+                .andExpect(status().isBadRequest());
+    }
 }
+
