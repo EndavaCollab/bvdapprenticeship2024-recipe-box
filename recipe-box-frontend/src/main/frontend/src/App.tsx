e2d65e55a689a5b3eec94163e6daea2e6b1efeed
AlinFlorescu80 <alin.florescu80@gmail.com>
2024-08-13 11:23:02 +0300

    Cleaned up

--- recipe-box-frontend/src/main/frontend/src/App.tsx
+++ recipe-box-frontend/src/main/frontend/src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RecipesList from './components/RecipesList';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipesList from "./components/RecipesList";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/recipeslist" element={<RecipesList/>} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/recipeslist" element={<RecipesList />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- recipe-box-frontend/src/main/frontend/src/components/HomePage.tsx
+++ recipe-box-frontend/src/main/frontend/src/components/HomePage.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import './HomePage.css';
-import {ReactComponent as Logo} from '../assets/images/recipe_box_logo_light.svg'
+import "./HomePage.css";
+import { ReactComponent as Logo } from "../assets/images/recipe_box_logo_light.svg";
 
 function HomePage() {
-  const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
-  const navigateToRecipesList = () => {
-    navigate('/recipeslist'); 
-  };
+    const navigateToRecipesList = () => {
+        navigate("/recipeslist");
+    };
 
-
-  return ( 
-    <div className="home-page">
-      <Logo className="logo"></Logo>
-      <h2>
-      Recipe Box is an intuitive app for organizing, discovering,
-      and sharing recipes. It helps food enthusiasts keep track of their
-      favorite dishes and explore new ones in a streamlined,
-      user-friendly interface.
-      </h2>
-      <button className="start-button" onClick={navigateToRecipesList}><h3>START COOKING</h3></button>
-
-    </div>
-  )
+    return (
+        <div className="home-page">
+            <Logo className="logo" />
+            <h2>
+                Recipe Box is an intuitive app for organizing, discovering, and
+                sharing recipes. It helps food enthusiasts keep track of their
+                favorite dishes and explore new ones in a streamlined,
+                user-friendly interface.
+            </h2>
+            <button className="start-button" onClick={navigateToRecipesList}>
+                <h3>START COOKING</h3>
+            </button>
+        </div>
+    );
 }
 
 export default HomePage;
