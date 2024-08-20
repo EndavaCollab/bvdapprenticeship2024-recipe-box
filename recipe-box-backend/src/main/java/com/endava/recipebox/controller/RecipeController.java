784d45e764d518bad31ae80d3a39cfbf8b665438
Muntean-Ionut <muntean.ionut98@yahoo.com>
2024-08-20 17:17:25 +0300

    Updated Recipe Endpoint

--- recipe-box-backend/src/main/java/com/endava/recipebox/controller/RecipeController.java
+++ recipe-box-backend/src/main/java/com/endava/recipebox/controller/RecipeController.java
@@ -1,51 +1,36 @@
 package com.endava.recipebox.controller;
 
-import com.endava.recipebox.model.MealType;
-import com.endava.recipebox.response.CustomRecipeMapper;
+
+import com.endava.recipebox.response.CustomRecipe;
 import com.endava.recipebox.service.RecipeService;
-import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 
+import java.util.List;
 
 
 @RestController
-@RequestMapping(path = "/recipe_box", produces = "application/json")
+@RequestMapping(path = "/recipes", produces = "application/json")
 public class RecipeController {
 
-    public final RecipeService recipeService;
-    private final CustomRecipeMapper customRecipeMapperMapper = Mappers.getMapper(CustomRecipeMapper.class);
+    private final RecipeService recipeService;
 
     @Autowired
     RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
-    @GetMapping("/publicRecipes")
-    public ResponseEntity<?> getAllPublicRecipes() throws IOException {
-        return ResponseEntity.ok(customRecipeMapperMapper.map(recipeService.getAllPublicRecipes()));
+    @GetMapping("/")
+    public ResponseEntity<List<CustomRecipe>> getAllPublicRecipes(@RequestParam(required = false) Long id) {
+        return ResponseEntity.ok(recipeService.getAllPublicRecipes());
     }
 
-    @GetMapping("/publicRecipes/{name}")
-    public ResponseEntity<?> getAllPublicRecipesByName(@PathVariable(name = "recipeName") String recipeName) {
-        if(recipeName == null)
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("The recipe name entered is null"));
-        return ResponseEntity.ok(customRecipeMapperMapper.map(recipeService.getAllPublicRecipesByName(recipeName)));
+    @GetMapping("/search")
+    public ResponseEntity<List<CustomRecipe>> getAllPublicRecipesByName(@RequestParam String recipeName) {
+        return ResponseEntity.ok(recipeService.getAllPublicRecipesByName(recipeName));
     }
 
-    @GetMapping("/publicRecipes/{mealType}")
-    public ResponseEntity<?> getAllPublicRecipesByType(@PathVariable(name = "mealType") MealType mealType) {
-        if(mealType == null)
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("The recipe type entered is null"));
-        return ResponseEntity.ok(customRecipeMapperMapper.map(recipeService.getAllPublicRecipesByType(mealType)));
-
-    }
 
 }
--- recipe-box-backend/src/main/java/com/endava/recipebox/service/RecipeService.java
+++ recipe-box-backend/src/main/java/com/endava/recipebox/service/RecipeService.java
@@ -1,55 +1,47 @@
 package com.endava.recipebox.service;
 
 
-import com.endava.recipebox.model.MealType;
 import com.endava.recipebox.model.Recipe;
 import com.endava.recipebox.model.RecipeStatus;
 import com.endava.recipebox.repository.RecipeRepository;
+import com.endava.recipebox.response.CustomRecipe;
+import com.endava.recipebox.response.CustomRecipeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final CustomRecipeMapper customRecipeMapper;
 
 
     @Autowired
-    public RecipeService(RecipeRepository recipeRepository) {
+    public RecipeService(RecipeRepository recipeRepository, CustomRecipeMapper customRecipeMapper) {
         this.recipeRepository = recipeRepository;
+        this.customRecipeMapper = customRecipeMapper;
     }
 
     public static boolean isPublic(Recipe recipe) {
         return recipe.getRecipeStatus() == RecipeStatus.PUBLIC;
     }
 
-    public List<Recipe> getAllPublicRecipes() {
-        List<Recipe> recipes = List.copyOf(recipeRepository.findAll()).stream()
+    public List<CustomRecipe> getAllPublicRecipes() {
+        return customRecipeMapper.map(recipeRepository.findAll().stream()
                 .filter(RecipeService::isPublic)
-                .collect(Collectors.toList());
-        return recipes;
+                .collect(Collectors.toList()));
     }
 
-    public List<Recipe> getAllPublicRecipesByName(String recipeName) {
-        List<Recipe> recipes = recipeRepository.findAll().stream()
+    public List<CustomRecipe> getAllPublicRecipesByName(String recipeName) {
+        return  customRecipeMapper.map(recipeRepository.findAll().stream()
                 .filter(RecipeService::isPublic)
-                .filter(r -> Objects.equals(r.getName(), recipeName))
-                .collect(Collectors.toList());
-        return  recipes;
-    }
-
-    public List<Recipe> getAllPublicRecipesByType(MealType mealType) {
-        List<Recipe> recipes = recipeRepository.findAll().stream()
-                .filter(RecipeService::isPublic)
-                .filter(r -> r.getMealType() == mealType)
-                .collect(Collectors.toList());
-        return recipes;
+                .filter(r -> r.getName().equals(recipeName))
+                .collect(Collectors.toList()));
     }
     
 }
