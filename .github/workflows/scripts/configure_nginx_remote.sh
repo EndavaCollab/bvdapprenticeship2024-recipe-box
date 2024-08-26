ce01bb6e230ddbed22732d6b8a4cb145f24d38fe
Vlad-Stefan Sas <me@svs.pm>
2024-08-26 18:10:07 +0300

    fix: (8) move some scripts and configs to separate files

---
+++ .github/workflows/scripts/configure_nginx_remote.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Move the nginx config file to the remote
+scp -r ../configs/* rcb:/tmp/configs/*
+
+ssh rcb <<CONTENT
+    # Install dependencies if required
+    sudo apt -qq update
+    sudo apt -qq install nginx -y
+
+    # Remove site files from previous builds
+    sudo rm -rf /etc/nginx/sites-enabled/*
+    sudo rm -rf /var/www/$GITHUB_FE_SITE_DOMAIN/html/*
+
+    # Create necessary folders and change permissions
+    sudo mkdir -p /var/www/$GITHUB_FE_SITE_DOMAIN/html
+    sudo chown -R $GITHUB_ADMIN_USERNAME:$GITHUB_ADMIN_USERNAME /var/www/$GITHUB_FE_SITE_DOMAIN/html
+    sudo chmod -R 755 /var/www/$GITHUB_FE_SITE_DOMAIN
+            
+    # Modify nginx script so that the domain is replaced and move it into the corresponding folder
+    sed -i "s/REPLACE_DOMAIN/$GITHUB_FE_SITE_DOMAIN/g" /tmp/configs/nginx.conf
+    sudo mv /tmp/configs/nginx.conf /etc/nginx/sites-available/$GITHUB_FE_SITE_DOMAIN
+
+    # Symlink site from available to enabled
+    sudo ln -s /etc/nginx/sites-available/$GITHUB_FE_SITE_DOMAIN /etc/nginx/sites-enabled/
+CONTENT
