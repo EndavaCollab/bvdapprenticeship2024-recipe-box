8e72425e27863557107f8c4eb99a71de885aa2a7
Vlad-Stefan Sas <me@svs.pm>
2024-09-04 16:32:45 +0300

    fix: (17) quoted environment values

--- .github/workflows/scripts/start_backend.sh
+++ .github/workflows/scripts/start_backend.sh
@@ -1,4 +1,4 @@
-screen -S "rcb-be" -X stuff "export DB_URL=$1$(printf \\r)"
-screen -S "rcb-be" -X stuff "export DB_USERNAME=$2$(printf \\r)"
-screen -S "rcb-be" -X stuff "export DB_PASSWORD=$3$(printf \\r)"
+screen -S "rcb-be" -X stuff "export DB_URL='$1'$(printf \\r)"
+screen -S "rcb-be" -X stuff "export DB_USERNAME='$2'$(printf \\r)"
+screen -S "rcb-be" -X stuff "export DB_PASSWORD='$3'$(printf \\r)"
 screen -S "rcb-be" -X stuff "java -jar ./recipe-box-backend-0.0.1-SNAPSHOT.jar$(printf \\r)"
