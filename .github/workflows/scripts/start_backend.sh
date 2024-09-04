4a4f3994da80d5ccb42553874b70332cb58cfd9d
Vlad-Stefan Sas <me@svs.pm>
2024-09-04 16:36:07 +0300

    fix: (17) fixed paths

--- .github/workflows/scripts/start_backend.sh
+++ .github/workflows/scripts/start_backend.sh
@@ -1,3 +1,4 @@
+cd ~/rcb-be/
 screen -S "rcb-be" -X stuff "export DB_URL='$1'$(printf \\r)"
 screen -S "rcb-be" -X stuff "export DB_USERNAME='$2'$(printf \\r)"
 screen -S "rcb-be" -X stuff "export DB_PASSWORD='$3'$(printf \\r)"
