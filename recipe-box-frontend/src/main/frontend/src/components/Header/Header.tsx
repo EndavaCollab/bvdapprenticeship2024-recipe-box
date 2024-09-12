9166647c21fe01bb2b36d08925da1ddfb2b983f0
Ionel14 <53469111+Ionel14@users.noreply.github.com>
2024-09-12 16:06:04 +0300

    added navigation buttons

--- recipe-box-frontend/src/main/frontend/src/components/Header/Header.tsx
+++ recipe-box-frontend/src/main/frontend/src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/images/recipe_box_logo_light.svg';
 
 const Header: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <header className="header">
             <nav>
@@ -12,6 +13,23 @@ const Header: React.FC = () => {
                         <li className="logo">
                             <Logo className="logo-image" />
                         </li>
+                        <li >
+                            <button 
+                                className="nav-item" 
+                                onClick={() => setIsOpen(!isOpen)}
+                            >
+                                Recipes &#x25BE;
+                            </button>
+                            {isOpen && (
+                                <ul className="dropdown-menu">
+                                    <li style={{textAlign:"left"}} ><Link to="/recipes/list" className="dropdown-item">Public Recipes</Link></li>
+                                    <li><Link to="/recipes/my-list" className="dropdown-item">My Recipes</Link></li>
+                                    <li><Link to="/recipes/ingredients" className="dropdown-item">Recipes with My Ingredients</Link></li>
+                                </ul>
+                            )}
+                        </li>
+                        <li><Link to="/ingredients" className="nav-item">Ingredients</Link></li>
+                        <li><Link to="/meal-plans" className="nav-item">Meal Plans</Link></li>
                     </div>
                     <div className="right-items">
                         <li className="right">
