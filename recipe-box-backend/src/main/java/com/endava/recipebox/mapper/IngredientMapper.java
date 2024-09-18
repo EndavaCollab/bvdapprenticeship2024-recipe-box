f2309be808b717f8c5ca7bc04162b130bd931550
SocaciVictor <147531728+SocaciVictor@users.noreply.github.com>
2024-09-18 12:41:23 +0300

    Added Ingredient Controller
    I created the endpoint to get all the ingredients.

---
+++ recipe-box-backend/src/main/java/com/endava/recipebox/mapper/IngredientMapper.java
@@ -0,0 +1,18 @@
+package com.endava.recipebox.mapper;
+
+import com.endava.recipebox.dto.IngredientsAllRequestDTO;
+import com.endava.recipebox.model.Ingredient;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface IngredientMapper {
+
+    @Mapping(source = "id", target = "ingredientId")
+    @Mapping(source = "name", target = "ingredientName")
+    IngredientsAllRequestDTO toIngredientsAllRequestDTO(Ingredient ingredient);
+
+    List<IngredientsAllRequestDTO> toIngredientsAllRequestDTOList(List<Ingredient> ingredients);
+}
