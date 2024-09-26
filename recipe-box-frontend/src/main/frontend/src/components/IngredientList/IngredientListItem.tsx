10ba1835bd29c52d724db8264730d8fe9898ab66
Alin Florescu <alin.florescu80@gmail.com>
2024-09-26 22:06:14 +0300

    Added a non-functional design

--- recipe-box-frontend/src/main/frontend/src/components/IngredientList/IngredientListItem.tsx
+++ recipe-box-frontend/src/main/frontend/src/components/IngredientList/IngredientListItem.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import './IngredientListItem.css';
-import {ReactComponent as Delete} from "../../assets/icons/delete-filled.svg";
-import {ReactComponent as Edit} from "../../assets/icons/edit.svg";
+import React, { useState, ChangeEvent } from "react";
+import "./IngredientListItem.css";
+import { ReactComponent as Delete } from "../../assets/icons/delete-filled.svg";
+import { ReactComponent as Edit } from "../../assets/icons/edit.svg";
+import { ReactComponent as CloseButton } from "../../assets/icons/close.svg";
 
 interface Ingredient {
     id: number;
@@ -20,48 +21,118 @@ interface IngredientItemProps {
 }
 
 const IngredientItem: React.FC<IngredientItemProps> = ({ ingredient }) => {
-    const { name, category, unit, kcal, carbs, fat, protein, quantity } = ingredient;
+    const [addPopupToggled, setAddPopupToggled] = useState(false);
+    const [popupIngredientQuantity, setPopupIngredientQuantity] =
+        useState<Number>(0);
+
+    const { name, category, unit, kcal, carbs, fat, protein, quantity } =
+        ingredient;
 
     const getQuantityClass = (quantity: number) => {
-        if (quantity < 10) return 'low';
-        if (quantity < 100) return 'medium';
-        return 'high';
+        if (quantity < 10) return "low";
+        if (quantity < 100) return "medium";
+        return "high";
     };
 
     return (
-        <tr>
-            <td>#{ingredient.id}</td>
-            <td>{name}</td>
-            <td>{category}</td>
-            <td>{unit}</td>
-            <td>{kcal}</td>
-            <td>{carbs}</td>
-            <td>{fat}</td>
-            <td>{protein}</td>
-            <td>
-                <span className={`ingredient-quantity ${getQuantityClass(quantity)}`}>
-                    <span className="circle"></span>
-                    <span className='ingredient-quantity-text'>{quantity}</span>
-                </span>
-            </td>
-            <td>
-                <div className="button-container">
-                     <button className="ingredient-button">
-                        <div className="button-content">
-                            <Edit className="svg-button"/>
-                            Edit
+        <>
+            <tr>
+                <td>#{ingredient.id}</td>
+                <td>{name}</td>
+                <td>{category}</td>
+                <td>{unit}</td>
+                <td>{kcal}</td>
+                <td>{carbs}</td>
+                <td>{fat}</td>
+                <td>{protein}</td>
+                <td>
+                    <span
+                        className={`ingredient-quantity ${getQuantityClass(
+                            quantity
+                        )}`}
+                    >
+                        <span className="circle"></span>
+                        <span className="ingredient-quantity-text">
+                            {quantity}
+                        </span>
+                    </span>
+                </td>
+                <td>
+                    <div className="button-container">
+                        <button className="ingredient-button">
+                            <div
+                                className="button-content"
+                                onClick={() => setAddPopupToggled(true)}
+                            >
+                                <Edit className="svg-button" />
+                                Edit
+                            </div>
+                        </button>
+                        <button className="ingredient-button">
+                            <div className="button-content">
+                                <Delete className="svg-button" />
+                                Delete
+                            </div>
+                        </button>
+                    </div>
+                </td>
+            </tr>
+
+            {addPopupToggled && (
+                <div className="add-ingredient-popup-overlay">
+                    <div className="add-ingredient-popup">
+                        <CloseButton
+                            className="close-button"
+                            onClick={() => {
+                                setAddPopupToggled(false);
+                                setPopupIngredientQuantity(0);
+                            }}
+                        />
+                        <div className="add-ingredient-popup-title">
+                            Type the quantity that you want to add for
+                            ingredient {ingredient.id} ({ingredient.unit})
                         </div>
-                     </button>
-                     <button className="ingredient-button">
-                         <div className="button-content">
-                             <Delete className="svg-button"/>
-                             Delete
-                         </div>
-                     </button>
+                        <input
+                            value={
+                                popupIngredientQuantity
+                                    ? Number(popupIngredientQuantity)
+                                    : ""
+                            }
+                            type="text"
+                            className="add-ingredient-popup-input"
+                            placeholder="Add quantity"
+                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                                setPopupIngredientQuantity(
+                                    Number(e.target.value)
+                                )
+                            }
+                        ></input>
+                        <div className="add-ingredient-popup-buttons">
+                            <button
+                                className="add-ingredient-popup-add-btn"
+                                disabled={!popupIngredientQuantity}
+                                onClick={() => {
+                                    setAddPopupToggled(false);
+                                    setPopupIngredientQuantity(0);
+                                }}
+                            >
+                                ADD
+                            </button>
+                            <button
+                                className="add-ingredient-popup-cancel-btn"
+                                onClick={() => {
+                                    setAddPopupToggled(false);
+                                    setPopupIngredientQuantity(0);
+                                }}
+                            >
+                                CANCEL
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </td>
-        </tr>
+            )}
+        </>
     );
-}
+};
 
 export default IngredientItem;
